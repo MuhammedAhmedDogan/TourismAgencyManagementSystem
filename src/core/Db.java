a467291a8982bb15a0c7779f65a547de917479c7
Ahmed Dogan <114875634+MuhammedAhmedDogan@users.noreply.github.com>
2024-04-24 16:22:57 +0300

    v10

--- src/core/Db.java
+++ src/core/Db.java
@@ -1,22 +1,34 @@
 package core;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Db {
     // Singleton Design Pattern ( Değerlendirme formu 6 )
     private static Db instance = null;
     private Connection connection = null;
-    private final String DB_URL = "jdbc:postgresql://localhost:5432/tourismagency";
-    private final String DB_USERNAME = "postgres";
-    private final String DB_PASSWORD = "6121333";
 
     private Db() {
         try {
+            Properties props = new Properties();
+            FileInputStream in = new FileInputStream("database.properties");
+            props.load(in);
+            in.close();
+            String DB_URL = props.getProperty("db.url");
+            String DB_USERNAME = props.getProperty("db.user");
+            String DB_PASSWORD = props.getProperty("db.password");
             this.connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
